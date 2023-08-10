445c390c62da2b83aac83eb7998c278a16f6210e
Merve Karataş <mervekaratas_1999@hotmail.com>
2023-08-10 20:53:21 +0300

    delete

--- ShoppingList/ShoppingList/ViewModels/ProductAddViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ShoppingList.ViewModels
-{
-    public class ProductAddViewModel
-    {
-        public string ProductName { get; set; } = null!; //uniq olmalı
-        public IFormFile? Image { get; set; } //ikon kullanabilirsin belli bir markayı işaret etmemeli
-        public string  CategoryName { get; set; }
-    }
-}
